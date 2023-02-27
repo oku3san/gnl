a817da8fa7566675ac20a7c236322ad7f98d56a0
oku3san <takaya0303@gmail.com>
2023-02-27 22:50:37 +0900

    Update

--- cmd/main.go
+++ cmd/main.go
@@ -2,33 +2,43 @@ package main
 
 import (
     "flag"
-    "fmt"
+    "log"
+    "os"
+    "os/exec"
 )
 
 func main() {
     // コマンドライン引数をパースする
     opt := flag.String("opt", "", "option setting")
     flag.Parse()
-    fmt.Println(*opt)
+    if string(*opt) == "diff" {
+        command := exec.Command("git", "diff")
+        command.Stdout = os.Stdout
+        err := command.Run()
+        if err != nil {
+            log.Fatal("git diff の実行に失敗しました")
+        }
+    } else {
+        command := exec.Command("git", "add", "-A")
+        command.Stdout = os.Stdout
+        err := command.Run()
+        if err != nil {
+            log.Fatal("git add の実行に失敗しました")
+        }
 
-    //command := exec.Command("git", "add", "-A")
-    //command.Stdout = os.Stdout
-    //err := command.Run()
-    //if err != nil {
-    //    log.Fatal("git add の実行に失敗しました")
-    //}
-    //
-    //command = exec.Command("git", "commit", "-m", "Update")
-    //command.Stdout = os.Stdout
-    //err = command.Run()
-    //if err != nil {
-    //    log.Fatal("git commit の実行に失敗しました")
-    //}
-    //
-    //command = exec.Command("git", "push", "origin", "main")
-    //command.Stdout = os.Stdout
-    //err = command.Run()
-    //if err != nil {
-    //    log.Fatal("git push の実行に失敗しました")
-    //}
+        command = exec.Command("git", "commit", "-m", "Update")
+        command.Stdout = os.Stdout
+        err = command.Run()
+        if err != nil {
+            log.Fatal("git commit の実行に失敗しました")
+        }
+
+        command = exec.Command("git", "push", "origin", "main")
+        command.Stdout = os.Stdout
+        err = command.Run()
+        if err != nil {
+            log.Fatal("git push の実行に失敗しました")
+        }
+    }
+
 }
